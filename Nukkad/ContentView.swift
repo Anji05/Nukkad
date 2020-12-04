83f97381f9d56491ae65e188327fd6926b6bcda6
Anji05 <aggarwalanjali5@gmail.com>
2020-12-04 09:19:34 +0530

    First Commit, Project Created

---
+++ Nukkad/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Nukkad
+//
+//  Created by Anjali Aggarwal on 04/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Nukkad/NukkadApp.swift
@@ -0,0 +1,17 @@
+//
+//  NukkadApp.swift
+//  Nukkad
+//
+//  Created by Anjali Aggarwal on 04/12/20.
+//
+
+import SwiftUI
+
+@main
+struct NukkadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
